3ba9774286ec44f05931f23fe592e366104380d9
Doug A Carter <doug.allen.carter@gmail.com>
2018-03-20 22:14:33 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Lovers+Quarrel" rel="stylesheet">  
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+      
+    <title>Hushed Thoughts</title>
+    
+    <style type="text/css">
+    
+        #title{
+            font-family: 'Lovers Quarrel', cursive;
+            font-size: 150px;
+            margin-top: 50px;
+            text-align: center;   
+        }
+        
+        #titleTwo{
+            font-family: 'Josefin Sans', sans-serif;
+            margin-top: 20px;
+            color: whitesmoke;
+            text-align: center;   
+        }
+        
+        #titleThree{
+            font-family: 'Josefin Sans', sans-serif;
+            text-align: center;
+
+        }
+        
+        #logInForm{
+            display: none;
+        }
+        
+        #diary{
+            width:100%;
+            height: 500px;
+        }
+        
+        #homePageContainer{
+            text-align: center;
+            width: 400px;
+            margin-top: 80px;
+        }
+        
+        #loggedInTitle{
+            font-family: 'Lovers Quarrel', cursive;
+        }
+        
+        #containerLoggedIn{
+            margin-top:200px;
+        }
+        
+        .whiteText{
+            color: whitesmoke;
+        }
+        
+        {
+            font-family: 'Lovers Quarrel', cursive;
+        }
+        
+        a, p{
+            color:white;
+            margin-top:20px;
+        }
+        
+        html { 
+            background: url(https://images.pexels.com/photos/159872/book-open-pages-literature-159872.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb) no-repeat center center fixed; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        
+        body{
+            background: none;
+        }
+     
+    </style>  
+      
+      
+  </head>
